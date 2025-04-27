7aee37bb42cc5d0bff64ec4bb205f3cfe9c25531
DIVYANSHPANDEY2003 <pandeydivyansh284@gmail.com>
2025-04-27 23:41:03 +0530

    Learning react

---
+++ 01basics_react_with_vite/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Pandey from "./pandey"
+
+function App() {
+
+  return (
+    <>
+      <h1>hello this is React with vite</h1>
+      <Pandey/>
+
+    </>
+  )
+}
+
+export default App
---
+++ 01basics_react_with_vite/src/pandey.jsx
@@ -0,0 +1,6 @@
+function Pandey(){
+    return(
+        <h2>Pandey is ready to learn react with vite </h2>
+    )
+}
+export default Pandey
